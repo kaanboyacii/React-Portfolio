75c255076f872d3dbda8b3f303c13dfcc656e873
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-10-22 11:31:27 +0300

    Framer motion tried

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Test from "./Test";
 import "./app.scss"
 import Navbar from "./components/navbar/Navbar";
 
@@ -13,6 +14,7 @@ const App = () => {
     <section>Hero</section>
     <section>Hero</section>
     <section>Hero</section>
+    {/* <Test></Test> */}
   </div>;
 };
 
