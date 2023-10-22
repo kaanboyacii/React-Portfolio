ca95cbcf18a6919230718e8c82b69b3db7fc298a
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-10-22 12:31:58 +0300

    Hero section animated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Test from "./Test";
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
 
 const App = () => {
   return (
     <div>
       <section id="Homepage">
         <Navbar />
+        <Hero />
       </section>
       <section id="Services">
       </section>
