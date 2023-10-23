7e7b14ed44311a18b817977d37aa5a9d09c604fb
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-10-23 19:17:27 +0300

    Services section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
 
 const App = () => {
   return (
@@ -11,14 +12,14 @@ const App = () => {
         <Navbar />
         <Hero />
       </section>
-      <section id="Services"> <Parallax type="services"/>
+      <section id="Services">
+        <Parallax type="services" />
       </section>
       <section>
+        <Services/>
       </section>
-      <section id="Portfolio">
-      </section>
-      <section id="Contact">
-      </section>
+      <section id="Portfolio"></section>
+      <section id="Contact"></section>
       {/* Framer Motion Crash Course */}
       {/* <Test/>
     <Test/> */}
